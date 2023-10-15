386e3adb73668cd766efa833ea5b3515a28e66c2
vincenpt <vincentp@seapine.com>
2023-10-14 23:50:42 -0400

    ESLint Warnings

--- src/mqtt/omg_devices/device.ts
+++ src/mqtt/omg_devices/device.ts
@@ -1,19 +1,28 @@
-
 /**
  * List of supported OMG Device types. Further definitions for these types should be done in their own file.
  */
 export enum KnownType {
-  AcuriteTower = "Acurite-Tower",
-  Acurite5n1 = "Acurite-5n1",
-  AcuriteProIn = "Acurite-00276rm"
+  AcuriteTower = 'Acurite-Tower',
+  Acurite5n1 = 'Acurite-5n1',
+  AcuriteProIn = 'Acurite-00276rm'
 }
 
 /**
  * Common properties for all OMG Devices
  */
 export interface IOMGDeviceBase {
+  /**
+   * Device ID
+   */
   id: string;
+  /**
+   * The model of the device. While this is set to KnownType, it is highly likely that the type will NOT be one of the
+   * listed known types.
+   */
   model: KnownType;
+  /**
+   * Signal strength
+   */
   rssi: number;
 }
 
