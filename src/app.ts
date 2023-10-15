76e35f703b3ad16f9fe1a2d655c2784d984a38d2
vincenpt <vincentp@seapine.com>
2023-10-15 11:55:51 -0400

    ESLint warnings

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,12 @@ import configuration from './services/configuration';
 import dataCache, { DataEntry } from './services/datacache';
 import { IOMGDeviceBase } from './mqtt/omg_devices/device';
 
-const log = configuration.log.extend("app");
+const log = configuration.log.extend('app');
 
+/**
+ * Starts the MQTT client
+ * @returns - A promise that resolves when finished.
+ */
 async function startMQTT(): Promise<void> {
   await mqttComms.startClient(configuration.mqttHost, {
     username: configuration.mqttUser,
@@ -13,12 +17,17 @@ async function startMQTT(): Promise<void> {
   });
 }
 
-function processTopic(topic: string, message: Buffer) {
+/**
+ * Process the provided MQTT message.
+ * @param topic - Topic for the message
+ * @param message - Received MQTT message
+ */
+function processTopic(topic: string, message: Buffer): void {
   const jsonConfig = message.toString();
   try {
     const messageObj = JSON.parse(jsonConfig);
 
-    if (messageObj.hasOwnProperty("id")) {
+    if (Object.hasOwn(messageObj, 'id')) {
       const device = messageObj as  IOMGDeviceBase;
       const dataEntry = new DataEntry(topic, device);
       log(`[${topic}] => IOMGDevice: ${device.model}\t${device.id}`);
@@ -33,19 +42,25 @@ function processTopic(topic: string, message: Buffer) {
 
 }
 
+/**
+ * Subscribe to the configured MQTT topic
+ */
 async function subscribe(): Promise<void> {
   if (mqttComms.isConnected()) {
-    const topic = configuration.mqttSrcTopic + "/#";
-    log("Subscribing to " + topic);
+    const topic = configuration.mqttSrcTopic + '/#';
+    log('Subscribing to ' + topic);
     await mqttComms.subscribe(topic, processTopic);
   }
 }
 
+/**
+ * Main startup function
+ */
 async function startup(): Promise<void> {
-  log("Starting MQTT client");
+  log('Starting MQTT client');
   await startMQTT();
 
-  log("Subscribing...");
+  log('Subscribing...');
   await subscribe();
 }
 
